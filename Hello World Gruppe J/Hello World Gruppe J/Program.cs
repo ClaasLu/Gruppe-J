5cead72e85a1a4bc3866d6cde630d798eb1932fd
ClaasLu <claas.luebbers@student.jade-hs.de>
2021-03-11 11:52:49 +0100

    Hello World Programm erstellt

---
+++ Hello World Gruppe J/Hello World Gruppe J/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_Gruppe_J
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt")
+        }
+    }
+}
