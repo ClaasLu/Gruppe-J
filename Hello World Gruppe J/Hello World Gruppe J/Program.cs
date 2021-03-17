d9f8d8edc423fcb6310f40d47d992883cd0f7e56
ClaasLu <claas.luebbers@student.jade-hs.de>
2021-03-17 11:28:41 +0100

    Konflikt behoben

--- Hello World Gruppe J/Hello World Gruppe J/Program.cs
+++ Hello World Gruppe J/Hello World Gruppe J/Program.cs
@@ -13,11 +13,10 @@ namespace Hello_World_Gruppe_J
 
             Console.WriteLine("Hallo Welt");
 
-<<<<<<< HEAD
-            Console.WriteLine("drücke beliebige Taste zum beenden!"); //hallo
-=======
-            Console.WriteLine("Drücke beliebige Taste zum beenden!");
->>>>>>> 6b132d9dc77ebc9f57a524b2eb3038ba036c0156
+
+
+            Console.WriteLine("Drücke beliebige Taste zum beenden!"); //Hallo
+
 
             Console.ReadKey();
             
